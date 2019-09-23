b890a7c293bde61466b63bf8f5c0cf873edc4033
jwthrs <jamie.weathers@cdiscovery.io>
2019-09-22 23:12:36 -0400

    Webscraper bug fixes; merged progress reports

--- src/app/Transcriber.py
+++
@@ -1,5 +0,0 @@
-# TODO: Transcriber is responsible for selecting an API or module to acquire the transcriptions of audio files.
-from api.google import CloudSpeechToText as cstt
-from api.apple import iTunesPodcastRSS as itp
-
-print (cstt.fetchCloudSettings())
