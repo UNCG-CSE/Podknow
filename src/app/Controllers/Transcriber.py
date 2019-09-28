49540198587066834f1a5521941fb71d3d3caeee
jwthrs <jamie.weathers@cdiscovery.io>
2019-09-28 17:03:40 -0400

    Restructured app files

--- src/app/Controllers/Transcriber.py
+++
@@ -1,8 +0,0 @@
-# TODO: Transcriber is responsible for selecting an API or module to acquire the transcriptions of audio files.
-
-# TODO: Ahh fix this import catastrophe.
-#from api.google import CloudSpeechToText as cstt
-#from api.apple import iTunesPodcastRSS as itp
-
-
-#print (cstt.fetchCloudSettings())
--- src/app/Transcriber.py
+++ src/app/Transcriber.py
@@ -1,5 +1,5 @@
 # TODO: Transcriber is responsible for selecting an API or module to acquire the transcriptions of audio files.
-from api.google import CloudSpeechToText as cstt
-from api.apple import iTunesPodcastRSS as itp
+from controllers.api.google import CloudSpeechToText as cstt
+from controllers.api.apple import iTunesPodcastRSS as itp
 
 print (cstt.fetchCloudSettings())
