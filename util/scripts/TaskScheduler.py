b0540b87e733429c07badb36c2b57ad9ebd3406f
Vincent Yang <k_yang6@uncg.edu>
2019-10-06 20:34:27 -0400

    new commit on task scheduler and updated top 200 cast list

--- util/scripts/TaskScheduler.py
+++ util/scripts/TaskScheduler.py
@@ -2,6 +2,7 @@ import requests
 import json
 import sys
 import csv
+import pandas as pd
 response = requests.get("https://rss.itunes.apple.com/api/v1/us/podcasts/top-podcasts/all/200/explicit.json")
 jsonObject = response.content
 print(jsonObject)
@@ -26,22 +27,51 @@ headerList = ["kind",
 "url",
 "releaseDate",
 "artistName",
-"id"
+"genres",
+"id",
+"user"
 ]
+
+users = ["Jamie","Vincent","Chris","Jeremy","Harini"]
+
 finalList = []
-print(len(finalList))
+
 for item in filteredResults:
     row = []
     for key,value in item.items():
         if key in headerList:
-           row.append(value)
+            row.append(value)
     finalList.append(row)
+
 print(len(finalList))
-import pandas as pd
-import sys
-reload(sys)
-sys.setdefaultencoding('utf8')
-dataFrame = pd.DataFrame(finalList)
-dataFrame.columns=headerList
-print(dataFrame.head())
-dataFrame.to_csv("top200Podcast.csv")
+
+remaining=len(finalList)%len(users)
+
+print(remaining)
+
+eachUserList = int(len(finalList)/len(users))
+
+print(eachUserList)
+
+lastList = []
+
+
+for user in users:
+    for j in range(eachUserList):
+        item = finalList[0]
+        print(user)
+        item.append(user)
+        print(item)
+        lastList.append(item)
+        del finalList[0]
+
+for i in range(len(finalList)):
+    item = finalList[i]
+    item.append(users[i])
+    lastList.append(item)
+
+dataframe = pd.DataFrame(lastList)
+
+print(dataframe.head())
+
+dataframe.to_csv("top200Podcast.csv")
